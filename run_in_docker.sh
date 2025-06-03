d6c5b48f098f5923d1c9e3031d47bc148c2f3226
tsgoff <tobias.sgoff@sgoff.com>
2025-06-03 17:13:10 +0200

    Update run_in_docker.sh

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -3,5 +3,13 @@
 mkdir -p slack-archive_USERDATA
 docker build -t slack-archive:dev .
 
-docker run --mount type=bind,source="$(pwd)"/slack-archive_USERDATA,target=/slack-archive/slack-archive \
-  -it slack-archive:dev "$@"
+if [ -t 1 ]; then
+  DOCKER_TTY="-it"
+else
+  DOCKER_TTY=""
+fi
+
+#docker rm -f slack-archiver
+
+docker run --name slack-archiver --mount type=bind,source="$(pwd)"/slack-archive_USERDATA,target=/slack-archive/slack-archive \
+  $DOCKER_TTY slack-archive:dev "$@"
